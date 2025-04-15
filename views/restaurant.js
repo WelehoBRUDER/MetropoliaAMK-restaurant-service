46bc558f77d5e0e13bb341af4719c3d9b2ef83ee
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-15 10:41:49 +0300

    Meal display working mostly

--- views/restaurant.js
+++ views/restaurant.js
@@ -1,13 +1,16 @@
 import createHeader from "../components/header.js";
 import {getRestaurantById} from "../script/session.js";
 import {createMealDisplay} from "../components/meals.js";
-import {getDailyMeals} from "../routes/routes.js";
+import {getWeeklyMeals} from "../routes/routes.js";
 
 createHeader();
 
 const restaurantId = new URLSearchParams(window.location.search).get("id");
 const restaurant = getRestaurantById(restaurantId);
 const restaurantName = document.querySelector("#restaurant-name");
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+let meals = [];
+let currentWeekDay = days[new Date().getDay() - 1]; // Monday is 0, Sunday is 6
 
 const updateRestaurantName = () => {
   restaurantName.innerText = `${restaurant.company} | ${restaurant.name}`;
@@ -16,18 +19,35 @@ const updateRestaurantName = () => {
 const initDaySelector = () => {
   const daySelector = document.querySelector(".day-selector");
   Object.values(daySelector.children).forEach((day) => {
+    if (currentWeekDay === day.getAttribute("data-value")) {
+      day.classList.add("selected");
+    }
     day.addEventListener("click", (e) => {
-      const selectedDay = e.currentTarget.getAttribute("data-value");
-      console.log(selectedDay);
-      getAllMeals();
+      currentWeekDay = e.currentTarget.getAttribute("data-value");
+      document.querySelector(".selected").classList.remove("selected");
+      e.currentTarget.classList.add("selected");
+      getDailyMeals(currentWeekDay);
     });
   });
 };
 
+const getDailyMeals = (day) => {
+  const index = days.indexOf(day);
+  const mealsForDay = meals.days[index];
+  if (mealsForDay) {
+    createMealDisplay(mealsForDay);
+  } else {
+    document.querySelector(
+      ".current-meals"
+    ).innerHTML = `<p class="error-message">No meals available for ${day}</p>`;
+  }
+};
+
 const getAllMeals = async () => {
-  const meals = await getDailyMeals(restaurantId);
-  console.log(meals);
+  meals = await getWeeklyMeals(restaurantId);
+  getDailyMeals(currentWeekDay);
 };
 
 updateRestaurantName();
+getAllMeals();
 initDaySelector();
