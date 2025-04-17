3bcf65f0a723ea90aa387d5b57ff7e40145c9601
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-04-17 17:14:02 +0300

    changed backendurl

--- routes/variables.js
+++ routes/variables.js
@@ -1,5 +1,5 @@
 export const baseUrl = "https://media2.edu.metropolia.fi/restaurant/api/v1";
-export const backendUrl = "http://localhost:5000/api/";
+export const backendUrl = "http://10.120.32.84/api/";
 export const deployUrl =
   "https://users.metropolia.fi/~juhanaha/web-development/MetropoliaAMK-restaurant-service";
 export const prodUrl = "";
